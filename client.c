384eb28747128edf9a5d1c0db0d87af5c2714c11
PatrykSkrucha <patryk.skrucha@gmail.com>
2023-01-22 21:22:07 +0100

    fd

--- client.c
+++ client.c
@@ -1,17 +1,22 @@
 #include "libft/libft.h"
 
-int main()
+int main(int argc, char **argv)
 {
-	//if (argc < 3)
-	//{
-	//	ft_printf("Too few arguments");
-	//	return (1);
-	//}
-	//if (ft_atoi(argv[1]) < 0)
-	//{
-	//	ft_printf("Incorrect PID.");
-	//	return (1);
-	//}
-	ft_printf("cos tu robie niby");
+	int	i;
+	i = -1;
+	if (argc < 3)
+	{
+		ft_printf("Too few arguments");
+		return (1);
+	}
+	while(argv[1][++i])
+	{
+		if (argv[1][i] < 48 || argv[1][i] > 57)
+		{
+			ft_printf("Incorrect PID.");
+			return (1);
+		}
+	}
+	ft_printf("%i", ft_atoi("a123a"));
 	return (0);
 }
