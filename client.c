456b0c2f0dad0a232980fc4478c14dfa9715f897
PatrykSkrucha <patryk.skrucha@gmail.com>
2023-01-22 20:43:12 +0100

    some changes

--- client.c
+++ client.c
@@ -2,6 +2,16 @@
 
 int main(int argc, char **argv)
 {
-	
+	if (argc < 3)
+	{
+		ft_printf("Too few arguments");
+		return (1);
+	}
+	if (ft_atoi(argv[1]) < 0)
+	{
+		ft_printf("Incorrect PID.");
+		return (1);
+	}
 	ft_printf("cos tu robie niby");
+	return (0);
 }
